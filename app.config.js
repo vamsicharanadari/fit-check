c51489e70909310e8680c58a231d54886bcf4408
adarivamsi <adarivamsi@gmail.com>
2025-06-07 13:45:38 -0500

    Updated version.

--- app.config.js
+++ app.config.js
@@ -4,6 +4,7 @@ export default {
     expo: {
         name: 'fit-check',
         slug: 'fit-check',
+        version: '1.0.0',
         extra: {
             "eas": {
                 "projectId": "a8e39552-fb28-49ca-b23d-929f1cf3614a"
