4ef30f70d8a76bb9baea2bf70f8acb149da8b4c1
adarivamsi <adarivamsi@gmail.com>
2025-06-07 04:43:31 -0500

    Updated appconfig.

--- app.config.js
+++ app.config.js
@@ -1,9 +1,12 @@
 import 'dotenv/config';
 
 export default {
-  expo: {
-    extra: {
-      BASE_URL: process.env.BASE_URL,
+    expo: {
+        extra: {
+            "eas": {
+                "projectId": "a8e39552-fb28-49ca-b23d-929f1cf3614a"
+            },
+            BASE_URL: process.env.BASE_URL,
+        },
     },
-  },
 };
