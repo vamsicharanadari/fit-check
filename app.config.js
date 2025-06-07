f8437b013e2e1ec524f6ec25548b9cba26cafc49
adarivamsi <adarivamsi@gmail.com>
2025-06-07 04:46:47 -0500

    Updated app config.

--- app.config.js
+++ app.config.js
@@ -2,11 +2,16 @@ import 'dotenv/config';
 
 export default {
     expo: {
+        name: 'fit-check',
+        slug: 'fit-check',
         extra: {
             "eas": {
                 "projectId": "a8e39552-fb28-49ca-b23d-929f1cf3614a"
             },
             BASE_URL: process.env.BASE_URL,
         },
+        android: {
+            package: 'com.adarivamsi.fitcheck'
+        }
     },
 };
